b983c696d68b102086b9e2f5cbb7d535211f4853
AndresTerraza <133818134+andrests32@users.noreply.github.com>
2024-02-19 17:52:46 -0500

    Update index.jsx
    colocamos un div con la estructura para que aparezca el último producto de las ultimas compras..

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -4,6 +4,17 @@ function MyOrders () {
     return(
         <Layout>
             My Orders
+            <div className="px-2 overflow-y-scroll flex-1">
+        {context.order?.slice(-1)[0]((product) => (
+          <OrderCard
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            imageUrl={product.images}
+            price={product.price}
+          />
+        ))}
+      </div>
         </Layout>
     )
 }
